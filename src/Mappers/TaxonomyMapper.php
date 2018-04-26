1d5e56ca0aedf159170cc341ff9668b7f350f896
Sebastian Sibelle <sebastian@ethicaljobs.com.au>
2018-04-26 15:14:00 +1000

    Added the taxonomy mapper implementation

---
+++ src/Mappers/TaxonomyMapper.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace EthicalJobs\SDK\Mappers;
+
+use EthicalJobs\SDK\ApiClient;
+use Illuminate\Support\Collection;
+
+class TaxonomyMapper implements MapperInterface
+{
+    /**
+     * @var ApiClient
+     */
+    private $client;
+
+    /**
+     * @var TaxonomyMapper|Collection
+     */
+    private $dictionary;
+
+    /**
+     * Object constructor
+     *
+     * @param
+     */
+    public function __construct(ApiClient $client)
+    {
+        $this->client = $client;
+        $this->dictionary = $this->fetchTaxonomyDictionary();
+    }
+
+    /**
+     * Fetches taxonomy dictionary from the query
+     *
+     * @return $this
+     */
+    private function fetchTaxonomyDictionary() : Collection
+    {
+        $response = $this->client->appData();
+
+        return new Collection(array_get($response, 'data.taxonomies')) ?? new Collection;
+    }
+
+    /**
+     * Select taxonomy from array
+     *
+     * @param int $taxonomyId
+     * @param $type string The name of the taxonomy
+     * @return string
+    */
+    public function map(int $taxonomyId, string $type): string
+    {
+        return $this->dictionary[$type][$taxonomyId]['title'] ?? '';
+    }
+}
