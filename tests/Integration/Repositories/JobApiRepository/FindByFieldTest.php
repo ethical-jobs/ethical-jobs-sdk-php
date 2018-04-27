e683f58ed722b13f3bc755b0737384b63558a92d
Andrew McLagan <andrew@ethicaljobs.com.au>
2018-04-27 17:08:15 +1000

    Latest

--- tests/Integration/Repositories/JobApiRepository/FindByFieldTest.php
+++ tests/Integration/Repositories/JobApiRepository/FindByFieldTest.php
@@ -2,8 +2,14 @@
 
 namespace EthicalJobs\Tests\SDK\Repositories\JobApiRepository;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\MockHandler;
+use Illuminate\Support\Facades\App;
 use EthicalJobs\SDK\ApiClient;
 use EthicalJobs\SDK\Repositories\JobApiRepository;
+use EthicalJobs\Tests\SDK\Fixtures;
 
 class FindByFieldTest extends \EthicalJobs\Tests\SDK\TestCase
 {
@@ -13,11 +19,21 @@ class FindByFieldTest extends \EthicalJobs\Tests\SDK\TestCase
      */
     public function it_can_find_by_a_field()
     {
-        $api = resolve(ApiClient::class);
+        $responseStack = new MockHandler([
+            Fixtures\Responses::jobs(),
+        ]);
+
+        $guzzle = new Client(['handler' => HandlerStack::create($responseStack)]);            
+
+        App::instance(Client::class, $guzzle);
+
+        $api = App::make(ApiClient::class);
 
         $repository = new JobApiRepository($api);
 
-        
+        $response = $repository->findByField('status', 'APPROVED');
+
+        dd($response);
     }    
 
     // /**
